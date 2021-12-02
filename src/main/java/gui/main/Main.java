973cce190be89e813f0c850488eb65289e38bd6e
thinkpad-chalister <martelmey@pm.me>
2021-12-01 20:38:38 -0800

    addKeys() method; Object/Array/ValueNode switch; return Map<String, String> map = new HashMap<>() populated w/ value of "word" per object in JSON list;

--- src/main/java/gui/main/Main.java
+++ src/main/java/gui/main/Main.java
@@ -3,6 +3,9 @@ package gui.main;
 import code.main.Word;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.node.ValueNode;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -15,8 +18,7 @@ import okhttp3.Response;
 import java.io.IOException;
 import java.lang.reflect.Array;
 import java.text.BreakIterator;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main extends Application {
 
@@ -27,21 +29,44 @@ public class Main extends Application {
         //launch(args);
 
         // Main word gen test
-        Word word = new Word("commit");
-        System.out.println(word.toString());
+        //Word word = new Word("commit");
+        //System.out.println(word.toString());
 
         // test endpoints & deserialization
-//        OkHttpClient client = new OkHttpClient();
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        String url = "https://wordsapiv1.p.rapidapi.com/words/lovely/synonyms";
+        OkHttpClient client = new OkHttpClient();
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        // data muse
+        Request request = new Request.Builder()
+                .url("https://api.datamuse.com/words?rel_syn=wait")
+                .build();
+        Response response = client.newCall(request).execute();
+        JsonNode rootNode = objectMapper.readTree(response.body().string());
+        System.out.print(rootNode);
+
+        Map<String,String> map = new HashMap<>();
+        addKeys("", rootNode, map, new ArrayList<>());
+
+        map.entrySet()
+                .forEach(System.out::println);
+
+//        List<String> synonyms = new ArrayList<>();
+//        BreakIterator breakIterator = BreakIterator.getWordInstance();
+//        breakIterator.setText(rootNode);
+
+        // words api synonyms
 //        Request request = new Request.Builder()
-//                .url(url)
+//                .url("https://wordsapiv1.p.rapidapi.com/words/lovely/synonyms")
 //                .get()
 //                .addHeader("x-rapidapi-host", "wordsapiv1.p.rapidapi.com")
 //                .addHeader("x-rapidapi-key", "e852927068mshaf1458fd33faf58p1c06fcjsn9a05d5c4c695")
 //                .build();
 //        Response response = client.newCall(request).execute();
 //        JsonNode rootNode = objectMapper.readTree(response.body().string());
+//        JsonNode wordNode = rootNode.path("word");
+//        System.out.println(rootNode);
+//        System.out.println(wordNode);
+
 //        JsonNode synonymsNode = rootNode.path("synonyms");
 //        List<String> synonyms = new ArrayList<>();
 //        BreakIterator breakIterator = BreakIterator.getWordInstance();
@@ -55,6 +80,36 @@ public class Main extends Application {
 //            }
 //        }
 //        System.out.println(synonyms);
+    }
+
+    public static void addKeys(String currentPath, JsonNode jsonNode, Map<String,String> map, List<Integer> suffix) {
+        if(jsonNode.isObject()) {
+            ObjectNode objectNode = (ObjectNode) jsonNode;
+            Iterator<Map.Entry<String,JsonNode>> iter = objectNode.fields();
+            String pathPrefix = currentPath.isEmpty() ? "" : currentPath + "-";
+            while(iter.hasNext()) {
+                Map.Entry<String, JsonNode> entry = iter.next();
+                addKeys(pathPrefix + entry.getKey(), entry.getValue(), map, suffix);
+            }
+        } else if(jsonNode.isArray()) {
+            ArrayNode arrayNode = (ArrayNode) jsonNode;
+            for(int i = 0; i < arrayNode.size(); i++) {
+                suffix.add(i + 1);
+                addKeys(currentPath, arrayNode.get(i), map, suffix);
+                if(i + 1 < arrayNode.size()) {
+                    suffix.remove(arrayNode.size() - 1);
+                }
+            }
+        } else if(jsonNode.isValueNode()) {
+            if(currentPath.contains("-")) {
+                for(int i = 0; i < suffix.size(); i++) {
+                    currentPath += "-" + suffix.get(i);
+                }
+                suffix = new ArrayList<>();
+            }
+            ValueNode valueNode = (ValueNode) jsonNode;
+            map.put(currentPath, valueNode.asText());
+        }
     }
 
     @Override
