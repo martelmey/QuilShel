a54fff13db71f60262d5cad58707b839d839cafd
thinkpad-chalister <martelmey@pm.me>
2021-12-07 21:44:56 -0800

    createWord Button takes inputWord.getText() of TextField and creates a new Word.class, prints word.toString() to console

--- src/main/java/gui/main/WordController.java
+++ src/main/java/gui/main/WordController.java
@@ -1,10 +1,13 @@
 package gui.main;
 
+import code.main.Word;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class WordController {
 
@@ -24,4 +27,36 @@ public class WordController {
     private TextField inputWord;
     @FXML
     private Button createWord;
+
+    @FXML
+    public void initialize() {
+        createWord.setDisable(true);
+    }
+    /**
+     * onButtonClicked
+     * create Word object
+     * from contents of inputWord
+     */
+    @FXML
+    public void onButtonClicked() throws IOException {
+        /**
+         * TESTS
+         * 1. check it's a real word
+         */
+        Word word = new Word(inputWord.getText());
+        System.out.println(word.toString());
+        inputWord.clear();
+        createWord.setDisable(true);
+    }
+    /**
+     * handleKeyReleased
+     * enable createWord button
+     * if inputWord field is not empty
+     */
+    @FXML
+    public void handleKeyReleased() {
+        String text = inputWord.getText();
+        boolean disableButtons = text.isEmpty() || text.trim().isEmpty();
+        createWord.setDisable(disableButtons);
+    }
 }
